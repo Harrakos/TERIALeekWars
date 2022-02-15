0675426ec6d7a8b4fd183e27650ac135c9bd9c96
Thomas Juliat <thomas.juliat24@gmail.com>
2022-02-15 14:45:00 +0100

    basic State

--- leek-wars-generator/autoRun.sh
+++ leek-wars-generator/autoRun.sh
@@ -3,7 +3,7 @@ nbRun=10
 i=0
 while ((i<nbRun))
 do 
-	java -jar generator.jar test/scenario/scenario1.json > resultat.json
+	java -jar generator.jar test/scenario/scenario1.json > ../resultat.json
 	i=$((i+1))
 	echo Combat $i 
 done
