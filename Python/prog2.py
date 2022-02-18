c13fdf5142cb6ea7b08a2f634cd8484040e15fcc
AissAiss <lucasaissaoui201@gmail.com>
2022-02-18 19:01:03 +0100

    J'ai fais des test pour récupérer les actions, je vais modifier RL.py avec une première version de renforcement demain

--- Python/prog2.py
+++ Python/prog2.py
@@ -1,16 +1,20 @@
 import json
 import collections
 
-with open("resultat.json") as jsonFile:
+with open("resultat2.json") as jsonFile:
     jsonObject = json.load(jsonFile)
     jsonFile.close()
 
 fight = jsonObject['fight']['actions'][1]
 winner = jsonObject['winner']
 logs = jsonObject['logs']['0']#['1']
+logTest = jsonObject['logs']['0']["11"]
+
+#print(logTest)
 
 sortedLogs = collections.OrderedDict(sorted(logs.items()))
 
 for log in sortedLogs: 
-    print(log)
+    rl = str(sortedLogs[log][0][2])
+    print(rl)
 
